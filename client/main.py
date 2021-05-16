c57f123c6392550e6ae41cb794dc837a4b0e1c38
nikolasmelui <slak@samaradom.ru>
2021-05-16 13:44:30 +0300

    Update client

--- client/main.py
+++ client/main.py
@@ -1,9 +1,9 @@
 import requests
 
-BASE_URL = "https://loocalhost:3000/"
+BASE_URL = "http://localhost:3000/"
 
 def main_fun():
-  r = requests.get(BASE_URL)
+  r = requests.get(f'{BASE_URL}items/pokemons')
   return r.text
 
 
