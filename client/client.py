24abcbf4f34a58384623da76c7e0f40132aa3bd1
nikolasmelui <slak@samaradom.ru>
2021-05-17 09:06:06 +0300

    Update client with new methods

--- client/client.py
+++ client/client.py
@@ -15,9 +15,22 @@ class Application:
         r = requests.get(f'{self.url}/items/pokemons')
         return r.json()
 
+    def get_by_id(self, id):
+        r = requests.get(f'{self.url}/items/pokemons/{id}')
+        return r.json()
+
+    def know_more(self, name):
+        r = requests.get(f'{self.url}/custom/know-more/{name}')
+        return r.json()
 
 application = Application(BASE_URL)
-print(str(application))
 
 pokemons = application.get_all()
 [print(pokemon) for pokemon in pokemons['data']]
+
+# info = application.get_by_id(1)
+# print(info)
+
+# info = application.know_more('magmar')
+# print(info)
+
